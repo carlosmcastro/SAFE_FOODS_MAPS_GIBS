bdf7b0c0b5688bb69cafb583fd808b39333f40ca
carlosmcastro <56790554+carlosmcastro@users.noreply.github.com>
2020-05-31 20:15:43 -0300

    Addition of comments

--- GIBS_Download_Join.py
+++ GIBS_Download_Join.py
@@ -5,21 +5,27 @@ from PIL import Image
 url = 'https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/MODIS_Terra_CorrectedReflectance_TrueColor/default/2012-07-09/250m/6/{}/{}.jpg'
 path_tmp = 'residual'
 
+#The images are ordered in a queue;
+#taking the coordinate as the central data
 def matrix_base(row, column):	
 	for i in range(9):
 		queue.put((row+(i//3)-1, column+(i%3)-1, i+1))
 		
+#Finding maximum limit in columns and rows, to avoid overloadFinding 
+#maximum limit in columns and rows, to avoid overload
 def limit_max(row, column):
 	max_ = requests.get(url.format(row, column))
 	max_ = int(max_.text.split('maximum value is', 1)[-1].split('<')[0].strip())
 	limits.append(max_)
 	
+#Write the images on disk.
 def write_img(file, name):
 	if not os.path.exists(path_tmp):
 		os.mkdir('residual')
 	with open(f'{path_tmp}/{name}.jpg', 'wb') as f:
 		f.write(file)
 
+#Download the images, checking that they are within the limits, in order.
 def download_set(queue, limits):
 	while not queue.empty():
 		row, col, id_name = queue.get(block=True)
@@ -28,6 +34,7 @@ def download_set(queue, limits):
 			if response:
 				write_img(response.content, str(id_name))
 	
+#We joined the mosaic into a single navigation image.
 def join_mosaic(name):
 	width = 512
 	height = 512
@@ -43,24 +50,31 @@ def join_mosaic(name):
 
 	
 if __name__ == '__main__':
+	
+	#Maximum limits.
 	limits = []
 
+	#We generate a queue to manage the access to the memory of the processes.
 	manager = multiprocessing.Manager()
 	queue = manager.Queue()
 
+	#We use threads to speed up the search for boundaries.
 	thr_rows = threading.Thread(target=limit_max, args=('1000', '0')) #rows
 	thr_column = threading.Thread(target=limit_max, args=('0', '1000')) #columns
 	
 	thr_rows.start()
 	thr_column.start()	
 	
+	#While finding the maxima, we generate the base array of coordinates (row, column, id_name)
 	matrix_base(15, 50)
 
 	thr_rows.join()
 	thr_column.join()
 	
+	#We ordered, knowing that no matter the zoom, the columns in this projection will always be larger than the total rows.
 	limits.sort()
 	
+	#We downloaded the set of images using concurrence.
 	pro1 = multiprocessing.Process(target=download_set, args=(queue, limits) )
 	pro1.start()
 	
@@ -70,6 +84,7 @@ if __name__ == '__main__':
 	pro1.join()
 	pro2.join()
 	
+	#We collect the downloaded images, and save them with a 'name'.
 	join_mosaic('Total')
 	
 	
